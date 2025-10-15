51b52864b38f31968d116c23f60494692af8e026
k22036 <k22036kk@aitech.ac.jp>
2025-10-15 20:36:31 +0900

    add: LinuxRouterクラスを追加

--- mininet_nat_example.py
+++ mininet_nat_example.py
@@ -1,48 +1,54 @@
 #!/usr/bin/env python3
 from mininet.net import Mininet
-from mininet.node import OVSSwitch
+from mininet.node import Node, OVSSwitch
 from mininet.cli import CLI
 from mininet.log import setLogLevel, info
 from mininet.topo import Topo
 
+class LinuxRouter(Node):
+    """ホストをルータ化するためのノードクラス"""
+    def config(self, **params):
+        super().config(**params)
+        self.cmd('sysctl -w net.ipv4.ip_forward=1')
 
-class SingleSwitchTopo(Topo):
-    "シンプルなスイッチ + 2ホストのトポロジ"
+    def terminate(self):
+        self.cmd('sysctl -w net.ipv4.ip_forward=0')
+        super().terminate()
 
+class SimpleTopo(Topo):
     def build(self):
-        # スイッチ作成
+        # スイッチとNATルータを作成
         s1 = self.addSwitch('s1')
+        r1 = self.addNode('r1', cls=LinuxRouter, ip='10.0.0.1/8')
+
         # ホスト作成
-        h1 = self.addHost('h1')
-        h2 = self.addHost('h2')
-        # リンク作成
+        h1 = self.addHost('h1', ip='10.0.0.10/8', defaultRoute='via 10.0.0.1')
+        h2 = self.addHost('h2', ip='10.0.0.11/8', defaultRoute='via 10.0.0.1')
+
+        # リンク接続
         self.addLink(h1, s1)
         self.addLink(h2, s1)
-
+        self.addLink(s1, r1)
 
 def run():
     setLogLevel('info')
-
-    # トポロジ作成
-    topo = SingleSwitchTopo()
-    net = Mininet(topo=topo, switch=OVSSwitch,
-                  controller=None, autoSetMacs=True)
-
-    info('*** Adding NAT node\n')
-    nat0 = net.addNAT('nat0', connect=True)  # 自動でホスト外部接続を確保
+    topo = SimpleTopo()
+    net = Mininet(topo=topo, switch=OVSSwitch, controller=None)
     net.start()
 
-    # デフォルトゲートウェイ設定
-    info('*** Setting default routes for hosts\n')
-    for host in net.hosts:
-        host.cmd('ip route add default via 10.0.0.1')
+    r1 = net['r1']
 
-    info('*** Running CLI\n')
+    # r1の外側IF（ホスト共有ネットワークに接続）を有効化
+    # DHCPでIPを取得
+    info('*** Configuring NAT on router r1\n')
+    r1.cmd('dhclient r1-eth1')  # 外部IFからDHCPでIPを取得
+    r1.cmd('iptables -t nat -A POSTROUTING -o r1-eth1 -j MASQUERADE')
+    r1.cmd('iptables -A FORWARD -i r1-eth0 -o r1-eth1 -j ACCEPT')
+    r1.cmd('iptables -A FORWARD -i r1-eth1 -o r1-eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT')
+
+    info('*** Network ready, test connectivity\n')
     CLI(net)
-
-    info('*** Stopping network\n')
     net.stop()
-
 
 if __name__ == '__main__':
     run()
