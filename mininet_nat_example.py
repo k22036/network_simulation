f7360a79cb385e89a91ebed0c24bd1c0ffa3babe
k22036 <k22036kk@aitech.ac.jp>
2025-10-15 21:09:12 +0900

    refactor: 不要なクラスとコードを削除し、NAT設定を簡素化

--- mininet_nat_example.py
+++ mininet_nat_example.py
@@ -1,54 +1,48 @@
 #!/usr/bin/env python3
 from mininet.net import Mininet
-from mininet.node import Node, OVSSwitch
+from mininet.node import OVSSwitch
+from mininet.link import TCLink
 from mininet.cli import CLI
 from mininet.log import setLogLevel, info
-from mininet.topo import Topo
 
-class LinuxRouter(Node):
-    """ホストをルータ化するためのノードクラス"""
-    def config(self, **params):
-        super().config(**params)
-        self.cmd('sysctl -w net.ipv4.ip_forward=1')
 
-    def terminate(self):
-        self.cmd('sysctl -w net.ipv4.ip_forward=0')
-        super().terminate()
+def setup():
+    net = Mininet(controller=None, link=TCLink, switch=OVSSwitch)
 
-class SimpleTopo(Topo):
-    def build(self):
-        # スイッチとNATルータを作成
-        s1 = self.addSwitch('s1')
-        r1 = self.addNode('r1', cls=LinuxRouter, ip='10.0.0.1/8')
+    info("*** Adding switch\n")
+    s1 = net.addSwitch("s1", failMode="standalone")
 
-        # ホスト作成
-        h1 = self.addHost('h1', ip='10.0.0.10/8', defaultRoute='via 10.0.0.1')
-        h2 = self.addHost('h2', ip='10.0.0.11/8', defaultRoute='via 10.0.0.1')
+    info("*** Adding hosts\n")
+    h1 = net.addHost("h1", ip="10.0.0.10/8")
+    h2 = net.addHost("h2", ip="10.0.0.11/8")
 
-        # リンク接続
-        self.addLink(h1, s1)
-        self.addLink(h2, s1)
-        self.addLink(s1, r1)
+    info("*** Adding NAT router\n")
+    # connect=True により自動で s1 と接続
+    nat = net.addNAT(name="nat0", connect=True, inNamespace=False)
 
-def run():
-    setLogLevel('info')
-    topo = SimpleTopo()
-    net = Mininet(topo=topo, switch=OVSSwitch, controller=None)
+    net.addLink(h1, s1)
+    net.addLink(h2, s1)
+
+    info("*** Starting network\n")
     net.start()
 
-    r1 = net['r1']
+    nat_ip = nat.IP()
+    info(f"*** NAT IP address: {nat_ip}\n")
 
-    # r1の外側IF（ホスト共有ネットワークに接続）を有効化
-    # DHCPでIPを取得
-    info('*** Configuring NAT on router r1\n')
-    r1.cmd('dhclient r1-eth1')  # 外部IFからDHCPでIPを取得
-    r1.cmd('iptables -t nat -A POSTROUTING -o r1-eth1 -j MASQUERADE')
-    r1.cmd('iptables -A FORWARD -i r1-eth0 -o r1-eth1 -j ACCEPT')
-    r1.cmd('iptables -A FORWARD -i r1-eth1 -o r1-eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT')
+    info("*** Configuring default routes\n")
+    ip_route_add = f"ip route add default via {nat_ip}"
+    h1.cmd(ip_route_add)
+    h2.cmd(ip_route_add)
 
-    info('*** Network ready, test connectivity\n')
+    info("*** Enabling forwarding and NAT rules\n")
+    nat.cmd("sysctl -w net.ipv4.ip_forward=1")
+    nat.cmd("iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE")
+
+    info("*** Ready! Try 'h1 ping -c 3 8.8.8.8'\n")
     CLI(net)
     net.stop()
 
-if __name__ == '__main__':
-    run()
+
+if __name__ == "__main__":
+    setLogLevel("info")
+    setup()
