13f61985a9b4da6ceb618cad43fc10fde38af2c1
k22036 <k22036kk@aitech.ac.jp>
2025-10-16 21:25:54 +0900

    refactor: LinuxRouterクラスを追加し、NATネットワークの設定を改善

--- mininet_nat_example.py
+++ mininet_nat_example.py
@@ -1,53 +1,66 @@
 #!/usr/bin/env python3
 from mininet.net import Mininet
-from mininet.node import OVSSwitch
-from mininet.link import TCLink
+from mininet.node import Node
 from mininet.cli import CLI
 from mininet.log import setLogLevel, info
 
 
-def setup():
-    net = Mininet(controller=None, link=TCLink, switch=OVSSwitch)
+class LinuxRouter(Node):
+    def config(self, **params):
+        super(LinuxRouter, self).config(**params)
+        self.cmd('sysctl -w net.ipv4.ip_forward=1')
 
-    info("*** Adding switch\n")
-    s1 = net.addSwitch("s1", failMode="standalone")
+    def terminate(self):
+        self.cmd('sysctl -w net.ipv4.ip_forward=0')
+        super(LinuxRouter, self).terminate()
 
-    info("*** Adding hosts\n")
-    h1 = net.addHost("h1", ip="10.0.0.10/8")
-    h2 = net.addHost("h2", ip="10.0.0.11/8")
 
-    info("*** Adding NAT router\n")
-    # connect=True により自動で s1 と接続
-    nat = net.addNAT(name="nat0", connect=True, inNamespace=False)
+def run_nat_network():
+    setLogLevel('info')
 
+    net = Mininet()
+
+    info("*** Creating nodes\n")
+    h1 = net.addHost('h1', ip='192.168.0.1/24')
+    r1 = net.addHost('r1', cls=LinuxRouter)
+    s1 = net.addSwitch('s1')
+    # 明示的に connect=False
+    nat = net.addNAT('nat0', connect=False, ip='192.168.1.1/24')
+
+    info("*** Creating links\n")
     net.addLink(h1, s1)
-    net.addLink(h2, s1)
+    net.addLink(r1, s1, r1_intfs=0)  # s1 側インターフェースを intf0 に固定
+    net.addLink(r1, nat, r1_intfs=1)  # NAT へ直接接続
 
-    info("*** Starting network\n\n")
+    info("*** Starting network\n")
     net.start()
 
-    s1_ip = s1.IP()
-    info(f"*** Switch IP address: {s1_ip}\n")
+    # インターフェース取得
+    r1_intfs = r1.intfList()
+    lan_if = r1_intfs[0]  # s1 側
+    wan_if = r1_intfs[1]  # nat 側
 
-    nat_ip = nat.IP()
-    info(f"*** NAT IP address: {nat_ip}\n")
+    info(f"LAN interface: {lan_if}, WAN interface: {wan_if}\n")
 
-    info("*** Configuring default routes\n")
-    ip_route_add = f"ip route add default via {nat_ip}"
-    h1.cmd(ip_route_add)
-    h2.cmd(ip_route_add)
+    info("*** Configuring router\n")
+    r1.cmd(f'ip addr add 192.168.0.254/24 dev {lan_if}')
+    # NAT側と同じネットワークに設定
+    r1.cmd(f'ip addr add 192.168.1.2/24 dev {wan_if}')
+    r1.cmd('sysctl -w net.ipv4.ip_forward=1')
 
-    info("*** Enabling forwarding and NAT rules\n")
-    nat.cmd("sysctl -w net.ipv4.ip_forward=1")
-    nat_ext_if = nat.defaultIntf().__str__().split('-')[1]
-    info("NAT external interface: " + nat_ext_if + "\n")
-    nat.cmd(f"iptables -t nat -A POSTROUTING -o {nat_ext_if} -j MASQUERADE")
+    info("*** Setting routes\n")
+    h1.cmd('ip route add default via 192.168.0.254')
+    r1.cmd('ip route add default via 192.168.1.1')
 
-    info("*** Ready! Try 'h1 ping -c 3 8.8.8.8'\n")
+    info("*** NAT config\n")
+    nat.configDefault()  # NAT の設定反映
+    nat.cmd('ip addr add 192.168.1.1/24 dev nat0-eth0')
+
+    info("*** Network ready. Try ping or traceroute\n")
     CLI(net)
+
     net.stop()
 
 
-if __name__ == "__main__":
-    setLogLevel("info")
-    setup()
+if __name__ == '__main__':
+    run_nat_network()
